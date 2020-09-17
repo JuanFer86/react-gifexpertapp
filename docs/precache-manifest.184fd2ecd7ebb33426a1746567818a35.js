6b60bfb046161b7945f9e41c01e4c57e667792a4
JUAN FERNANDO NARVAEZ GALLEGO <narvaezju1@gmail.com>
2020-09-16 21:28:46 -0500

    docs agregado

---
+++ docs/precache-manifest.184fd2ecd7ebb33426a1746567818a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b7b15d1cfbdb8dae2ede8f126f0303",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4c38cf3bf305757771",
+    "url": "/static/css/2.2ce0d9b2.chunk.css"
+  },
+  {
+    "revision": "d0eabebf7a6eeca599a8",
+    "url": "/static/css/main.225a6ab0.chunk.css"
+  },
+  {
+    "revision": "6d4c38cf3bf305757771",
+    "url": "/static/js/2.8acd5d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8acd5d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0eabebf7a6eeca599a8",
+    "url": "/static/js/main.df4e971c.chunk.js"
+  },
+  {
+    "revision": "4e3a1f73b6e5de40b557",
+    "url": "/static/js/runtime-main.5c054f69.js"
+  }
+]);
